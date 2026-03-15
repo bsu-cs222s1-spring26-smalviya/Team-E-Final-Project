7fc327f5afca84231c3c844fed9abe78a591c436
AlecFeiYe <fei.ye@bsu.edu>
2026-03-15 14:11:51 -0400

    fix: Modify the usage of category to automatically determine whether to deposit or withdraw money based on the amount entered by the user.

--- src/main/java/Model/Transaction.java
+++ src/main/java/Model/Transaction.java
@@ -5,13 +5,18 @@ import java.time.LocalDateTime;
 public class Transaction {
     private double amount;
     private String category;
-    private LocalDateTime date;
+    private String date;
     private String description;
 
-    public Transaction(double amount, String category, String description) {
+    public Transaction(double amount, String description) {
         this.amount = amount;
-        this.category = category;
-        this.date = LocalDateTime.now();
+
+        if (amount < 0) {
+            this.category = "Withdrawal";
+        } else {
+            this.category = "Deposit";
+        }
+        this.date = LocalDateTime.now().toString();
         this.description = description;
     }
 
@@ -28,7 +33,7 @@ public class Transaction {
     }
 
     public String getDate() {
-        return date.toString();
+        return date;
     }
 
 }
