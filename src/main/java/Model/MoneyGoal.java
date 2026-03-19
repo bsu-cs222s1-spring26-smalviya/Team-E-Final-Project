7d313056ddca564b560041b896247265cdba8e09
AlecFeiYe <fei.ye@bsu.edu>
2026-03-18 20:47:28 -0400

    fix: Move the computation from User back to here.

--- src/main/java/Model/MoneyGoal.java
+++ src/main/java/Model/MoneyGoal.java
@@ -26,4 +26,11 @@ public class MoneyGoal {
     public String toString() {
         return "Goal: " + goalName + "\nTarget Amount: " + targetAmount + "\nDeadline: " + deadline;
     }
+
+    public double calculateCompletion(double currentBalance) {
+        if (targetAmount <= 0) {
+            return 0.0;
+        }
+        return (currentBalance / targetAmount) * 100.0;
+    }
 }
