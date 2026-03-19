bfe53a83ed5e07f1c2a68a86a140dfc440aa95f1
AlecFeiYe <fei.ye@bsu.edu>
2026-03-18 22:15:19 -0400

    refactor: Extract connection logic and fix resource leak.

--- src/main/java/Converter/CurrencyConverter.java
+++ src/main/java/Converter/CurrencyConverter.java
@@ -1,4 +1,5 @@
 package Converter;
+
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
@@ -7,28 +8,30 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class CurrencyConverter {
- public String getExchangeRateJson(String fromCurrency, String toCurrency)throws NetWorkException,APIException {
-     try {
-         URL ConverterURL = new URL("https://currencyrateapi.com/api/latest?base=" + fromCurrency + "&quote=" + toCurrency);
-         HttpURLConnection connection = (HttpURLConnection) ConverterURL.openConnection();
-         connection.setRequestMethod("GET");
-         int  responseCode = connection.getResponseCode();
-         if (responseCode != 200) {
-             throw new APIException("API request failed");
-         }
-         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-         String inputLine;
-         StringBuilder content = new StringBuilder();
-         while ((inputLine = bufferedReader.readLine()) != null) {
+    public String getExchangeRateJson(String fromCurrency, String toCurrency) throws NetWorkException, APIException {
+        try {
+            BufferedReader bufferedReader = getBufferedReader(fromCurrency, toCurrency);
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                content.append(inputLine);
+            }
+            return content.toString();
+        } catch (IOException e) {
+            throw new NetWorkException("network error");
+        }
+    }
 
-             content.append(inputLine);
+    private BufferedReader getBufferedReader(String fromCurrency, String toCurrency) throws IOException, APIException {
+        URL ConverterURL = new URL("https://currencyrateapi.com/api/latest?base=" + fromCurrency + "&quote=" + toCurrency);
+        HttpURLConnection connection = (HttpURLConnection) ConverterURL.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
 
-         }
-         return content.toString();
-     } catch (IOException e) {
-         throw new NetWorkException("nextwork error");
-     }
+        if (responseCode != 200) {
+            throw new APIException("API request failed");
+        }
 
-
- }
+        return new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    }
 }
