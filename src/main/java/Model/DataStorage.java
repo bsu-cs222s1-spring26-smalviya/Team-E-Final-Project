45b2795323afbca79b861e266a85625516b24890
AlecFeiYe <fei.ye@bsu.edu>
2026-03-18 20:04:59 -0400

    fix: When an error occurs, an exception is thrown, and the UI is no longer handled manually.

--- src/main/java/Model/DataStorage.java
+++ src/main/java/Model/DataStorage.java
@@ -6,49 +6,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import com.google.gson.Gson;
-import View.User;
 
 public class DataStorage {
     private Gson gson = new Gson();
-
-    public DataStorage() {
-
-    }
 
     public boolean checkLocalData(String username) {
         return new File(username + "_data.json").exists();
     }
 
-    public Account loadAccount(String username) {
-        String fileName = username + "_data.json";
-        File file = new File(fileName);
-        User ui = new User();
-
+    public Account loadAccount(String username) throws IOException {
+        File file = new File(username + "_data.json");
         if (!file.exists()) {
             return null;
         }
-
         try (FileReader reader = new FileReader(file)) {
-            Account account = gson.fromJson(reader, Account.class);
-            ui.displayMessage("Data successfully loaded for: " + username);
-
-            return account;
-        } catch (IOException e) {
-            ui.displayMessage("Failed to load data. Error: " + e.getMessage());
-            return null;
+            return gson.fromJson(reader, Account.class);
         }
     }
 
-    public void saveAccount(Account account) {
+    public void saveAccount(Account account) throws IOException {
         String fileName = account.getUsername() + "_data.json";
-        User ui = new User();
         try (FileWriter writer = new FileWriter(fileName)) {
             gson.toJson(account, writer);
-            ui.displayMessage("Data successfully saved.");
-        } catch (IOException e) {
-            ui.displayMessage("Failed to save data. Error:" + e.getMessage());
         }
-
-
     }
 }
