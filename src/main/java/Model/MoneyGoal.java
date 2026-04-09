f4a6b19228e7ed357c642d91ffcfcaab0829f471
AlecFeiYe <fei.ye@bsu.edu>
2026-04-09 10:24:56 -0400

    fix: According to the compiler's requirements, change "money goal" to "record" type.

--- src/main/java/Model/MoneyGoal.java
+++ src/main/java/Model/MoneyGoal.java
@@ -1,32 +1,9 @@
 package Model;
 
-public class MoneyGoal {
-    final private String goalName;
-    final private double targetAmount;
-    final private String deadline;
-
-    public MoneyGoal(String goalName, double targetAmount, String deadline) {
-        this.goalName = goalName;
-        this.targetAmount = targetAmount;
-        this.deadline = deadline;
-    }
-
-    public String getGoalName() {
-        return goalName;
-    }
-
-    public double getTargetAmount() {
-        return targetAmount;
-    }
-
-    public String getDeadline() {
-        return deadline;
-    }
-
+public record MoneyGoal(String goalName, double targetAmount, String deadline) {
     public String toString() {
         return "Goal: " + goalName + "\nTarget Amount: " + targetAmount + "\nDeadline: " + deadline;
     }
-
     public double calculateCompletion(double currentBalance) {
         if (targetAmount <= 0) {
             return 0.0;
