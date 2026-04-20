f755a6a1fcf2069400a32daef177d89345e1f4ae
LordRaymik <silasjohnson317@gmail.com>
2026-04-20 14:24:41 -0400

    Reworked MoneyGoal to actually function correctly and pass its tests. Hoping making it work didn't cause problems elsewhere.

--- src/main/java/Model/MoneyGoal.java
+++ src/main/java/Model/MoneyGoal.java
@@ -1,9 +1,34 @@
 package Model;
 
-public record MoneyGoal(String goalName, double targetAmount, String deadline) {
+import java.lang.classfile.instruction.ReturnInstruction;
+
+public class MoneyGoal {
+    private String goalName;
+    private double targetAmount;
+    private String deadline;
+
+    public MoneyGoal(String goalName, double targetAmount, String deadline) {
+        this.goalName = goalName;
+        this.targetAmount = targetAmount;
+        this.deadline = deadline;
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+
+    public double getTargetAmount() {
+        return targetAmount;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
     public String toString() {
         return "Goal: " + goalName + "\nTarget Amount: " + targetAmount + "\nDeadline: " + deadline;
     }
+
     public double calculateCompletion(double currentBalance) {
         if (targetAmount <= 0) {
             return 0.0;
